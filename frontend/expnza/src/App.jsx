6ba70654ae7d8b847770caf3a9fec67c5b247879
ipeng5 <ypeng115@gmail.com>
2025-08-21 14:18:10 +0200

    Create routes

--- frontend/expnza/src/App.jsx
+++ frontend/expnza/src/App.jsx
@@ -1,5 +1,33 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
+
 function App() {
-  return <div className="text-3xl text-teal-600">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Root />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/income" element={<Income />} />
+        <Route path="/expense" element={<Expense />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
+
+const Root = function () {
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
