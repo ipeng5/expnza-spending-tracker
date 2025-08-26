1de216b42f0b6c1c5573fe7e52b355f2f9383168
ipeng5 <ypeng115@gmail.com>
2025-08-26 22:00:27 +0200

    Create dashboard page layout

---
+++ frontend/expnza/src/hooks/useUserAuth.jsx
@@ -0,0 +1,39 @@
+import { useContext, useEffect } from "react";
+import { UserContext } from "../context/UserContext";
+import { useNavigate } from "react-router-dom";
+import { API_PATHS } from "../utils/apiPaths";
+import axiosInstance from "../utils/axiosInstance";
+
+export function useUserAuth() {
+  const { user, updateUser, clearUser } = useContext(UserContext);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) return;
+
+    let isMounted = true;
+
+    async function fetchUserInfo() {
+      try {
+        const response = await axiosInstance.get(API_PATHS.AUTH.GET_USER_INFO);
+
+        if (isMounted && response.data) {
+          updateUser(response.data);
+        }
+      } catch (err) {
+        console.error("Failed to fetch user info:", err);
+        if (isMounted) {
+          clearUser();
+          navigate("/login");
+        }
+      }
+    }
+
+    fetchUserInfo();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [updateUser, clearUser, navigate]);
+}
